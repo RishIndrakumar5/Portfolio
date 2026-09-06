3704160b4fdcb2871b07beb9f0fe3db30f775509
Rish <237796974+RishIndrakumar5@users.noreply.github.com>
2026-09-06 10:09:15 -0400

    Update certificates for portfolio site

--- certificates-data.js
+++ certificates-data.js
@@ -5,7 +5,7 @@ window.CERTIFICATES_DATA = {
     {
       "id": "cert-1788039052490-lv2v53",
       "title": "Honor Roll 6th Grade",
-      "description": "",
+      "description": "Awarded for Excellence in Academics",
       "image": "",
       "x": 4.9,
       "y": 18,
@@ -15,12 +15,22 @@ window.CERTIFICATES_DATA = {
     {
       "id": "cert-1788124174359-0562bo",
       "title": "High Honors 7th Grade 1",
-      "description": "",
+      "description": "Awarded after my first semester",
       "image": "",
       "x": 35.1,
       "y": 21,
       "width": 28,
       "zIndex": 6
+    },
+    {
+      "id": "cert-1788703530836-zreqce",
+      "title": "High Honors 7th Grade 2",
+      "description": "Awarded after my third quarter",
+      "image": "",
+      "x": 65,
+      "y": 20,
+      "width": 28,
+      "zIndex": 7
     }
   ]
 };
